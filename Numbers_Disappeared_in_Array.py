ddac31639ae3a5278cc6e37a236442f8b1d05e3f
ashutoshchandrajha <ashutoshchandrajha@users.noreply.github.com>
2017-04-10 16:03:41 -0400

    Update Numbers_Disappeared_in_Array.py

--- Numbers_Disappeared_in_Array.py
+++ Numbers_Disappeared_in_Array.py
@@ -12,3 +12,19 @@ class Solution(object):
             if l[i]==0:
                 r.append(i+1)
         return r
+
+    
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        r = []
+        for i in nums:
+            if nums[abs(i)-1]>0:
+                nums[abs(i)-1] = nums[abs(i)-1]*-1
+        for i in range(len(nums)):
+            if nums[i]>0:
+                r.append(i+1)
+        return r
