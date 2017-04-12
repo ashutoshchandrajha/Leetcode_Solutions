ce74e4f362f45d92622ceee7c9daa4fff47cd646
ashutoshchandrajha <ashutoshchandrajha@users.noreply.github.com>
2017-04-12 10:02:17 -0400

    Update Power_of_Two.py

--- Power_of_Two.py
+++ Power_of_Two.py
@@ -4,13 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n & (n-1) != 0:
-            return False
-        k = 0
-        num = n
-        while num!=0:
-            k+=1
-            num = num >> 1
-        if 2**(k-1)==n:
-            return True
-        return False
+        return n>0 and n&(n-1)==0
