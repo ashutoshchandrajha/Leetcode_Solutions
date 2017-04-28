20598c316c426d15c9282d6d16ccc9b2bb47e8c9
ashutoshchandrajha <ashutoshchandrajha@users.noreply.github.com>
2017-04-28 14:43:20 -0400

    Update First_Unique_Character_String.py

--- First_Unique_Character_String.py
+++ First_Unique_Character_String.py
@@ -14,3 +14,19 @@ class Solution(object):
             if len(d[s[i]])==1:
                 return i
         return -1
+
+    
+#another accepted solution
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = [0]*256
+        for i in s:
+            l[ord(i)]+=1
+        for i in range(len(s)):
+            if l[ord(s[i])]==1:
+                return i
+        return -1
